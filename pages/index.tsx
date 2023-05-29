24ce2e0ef0a479ae8913a90eec21bbc37e3d2827
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-05-29 16:52:02 +0530

    Complete MovieCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovielist from "@/hooks/useMovielist";
 import { NextPageContext } from "next"
 import { getSession } from "next-auth/react"
 
@@ -21,12 +23,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-
+  const { data: movies = [] } = useMovielist();
 
   return (
     <>
       <Navbar  />
       <Billboard />
+      <div className="pb-40">
+      <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }

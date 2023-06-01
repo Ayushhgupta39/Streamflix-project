58be59ca337fcc510b537a45884dac325544d4f2
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-06-01 11:32:34 +0530

    Favorite functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,39 @@
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useFavorites from "@/hooks/useFavorites";
 import useMovielist from "@/hooks/useMovielist";
-import { NextPageContext } from "next"
-import { getSession } from "next-auth/react"
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
   if (!session) {
-    return { 
+    return {
       redirect: {
         destination: "/auth",
         permanent: false,
-      }
-    }
+      },
+    };
   }
 
   return {
-    props : {} 
-  }
+    props: {},
+  };
 }
 
 export default function Home() {
   const { data: movies = [] } = useMovielist();
+  const { data: favorites = [] } = useFavorites();
 
   return (
     <>
-      <Navbar  />
+      <Navbar />
       <Billboard />
       <div className="pb-40">
-      <MovieList title="Trending Now" data={movies} />
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   );

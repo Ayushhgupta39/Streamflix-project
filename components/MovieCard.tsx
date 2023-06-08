5545139073fe0a4785fc1ae47f07c5d781917879
Ayush Gupta <ayushgupta3902@gmail.com>
2023-06-08 14:17:27 +0530

    Fix moviecard for mobile screens

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -13,9 +13,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const { openModal } = useInfoModal();
 
   return (
-    <div className="group bg-zinc-900 col-span relative h-[12vw]">
+    <div className="cursor-pointer group bg-zinc-900 col-span relative h-[12vw]">
       <img
-        className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"
+      onClick={() => router.push(`/watch/${data?.id}`)}
+        className="object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"
         src={data.thumbnailUrl}
         alt="movie-card"
       />
